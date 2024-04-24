be54d7fa1c1e18199faccadea030dbb39bb5f83c
Leo98ps <leo.leo.5555@live.com>
2024-04-23 22:45:33 -0300

    Adicionados os operadores matematicos

---
+++ operators/playground-microservices/src/main/java/br/com/playground/MathController.java
@@ -0,0 +1,171 @@
+package br.com.playground;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.playground.exceptions.UnsupportedMathOperationException;
+
+/**
+ * Controller for mathematical operations.
+ */
+@RestController
+public class MathController {
+
+	/**
+	 * Calculates the sum of two integers.
+	 * 
+	 * @param numberOne The first number to be added.
+	 * @param numberTwo The second number to be added.
+	 *
+	 * @return The sum of the two provided numbers.
+	 * 
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/add/{numberOne}/{numberTwo}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal add(/* */
+			@PathVariable(value = "numberOne") String numberOne, /* */
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if ((!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) /* */
+				|| (!StringUtils.hasText(numberTwo) && !numberTwo.matches("^-?\\d*\\.?\\d+$\r\n"))) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return new BigDecimal(numberOne)/* */
+				.add(new BigDecimal(numberTwo)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Subtracts the second number from the first number and returns the result.
+	 * 
+	 * @param numberOne The first number.
+	 * @param numberTwo The second number to subtract from the first.
+	 *
+	 * @return The result of subtracting the second number from the first.
+	 * 
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/subtract/{numberOne}/{numberTwo}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal subtract(/* */
+			@PathVariable(value = "numberOne") String numberOne, /* */
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if ((!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) /* */
+				|| (!StringUtils.hasText(numberTwo) && !numberTwo.matches("^-?\\d*\\.?\\d+$\r\n"))) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return new BigDecimal(numberOne)/* */
+				.subtract(new BigDecimal(numberTwo)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Multiplies the two provided numbers.
+	 * 
+	 * @param numberOne The first number.
+	 * @param numberTwo The second number to multiply with the first.
+	 * 
+	 * @return The result of multiplying the two numbers (numberOne * numberTwo).
+	 * 
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/multiply/{numberOne}/{numberTwo}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal multiply(/* */
+			@PathVariable(value = "numberOne") String numberOne, /* */
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if ((!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) /* */
+				|| (!StringUtils.hasText(numberTwo) && !numberTwo.matches("^-?\\d*\\.?\\d+$\r\n"))) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return new BigDecimal(numberOne)/* */
+				.multiply(new BigDecimal(numberTwo)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Divides the first number by the second number and returns the result.
+	 * 
+	 * @param numberOne The number to be divided.
+	 * @param numberTwo The number by which the dividend is to be divided.
+	 * 
+	 * @return The result of dividing the dividend by the divisor.
+	 * 
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/divide/{numberOne}/{numberTwo}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal divide(/* */
+			@PathVariable(value = "numberOne") String numberOne, /* */
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if ((!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) /* */
+				|| (!StringUtils.hasText(numberTwo) && !numberTwo.matches("^-?\\d*\\.?\\d+$\r\n"))) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return new BigDecimal(numberOne)/* */
+				.divide(new BigDecimal(numberTwo)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Calculates the average of the two provided numbers.
+	 * 
+	 * @param numberOne The first number.
+	 * @param numberTwo The second number.
+	 * 
+	 * @return The average of the two numbers ((numberOne + numberTwo) / 2).
+	 *
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/average/{numberOne}/{numberTwo}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal average(/* */
+			@PathVariable(value = "numberOne") String numberOne, /* */
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if ((!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) /* */
+				|| (!StringUtils.hasText(numberTwo) && !numberTwo.matches("^-?\\d*\\.?\\d+$\r\n"))) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return (new BigDecimal(numberOne).add(new BigDecimal(numberTwo)))/* */
+				.divide(BigDecimal.valueOf(2)).setScale(2, RoundingMode.HALF_UP);
+	}
+
+	/**
+	 * Calculates the square root of the provided number.
+	 * 
+	 * @param numberOne The number whose square root is to be calculated.
+	 * 
+	 * @return The square root of the provided number.
+	 *
+	 * @throws Exception If any other exception occurs.
+	 */
+	@RequestMapping(value = "/sqrt/{numberOne}", /* */
+			method = RequestMethod.GET)
+	public BigDecimal sqrt(/* */
+			@PathVariable(value = "numberOne") String numberOne) throws Exception {
+
+		if (!StringUtils.hasText(numberOne) && !numberOne.matches("^-?\\d*\\.?\\d+$\r\n")) {
+			throw new UnsupportedMathOperationException("Please set a numeric value");
+		}
+
+		return new BigDecimal(numberOne).sqrt(new MathContext(2));
+	}
+}
---
+++ operators/playground-microservices/src/main/java/br/com/playground/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,63 @@
+package br.com.playground.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.playground.exceptions.ExceptionResponse;
+import br.com.playground.exceptions.UnsupportedMathOperationException;
+
+/**
+ * CustomizedResponseEntityExceptionHandler is a custom exception handler that
+ * extends ResponseEntityExceptionHandler.
+ */
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler /* */
+		extends ResponseEntityExceptionHandler {
+
+	/**
+	 * Handles all types of exceptions thrown within the application.
+	 * 
+	 * @param ex      The exception to be handled.
+	 * @param request The HTTP request that resulted in the exception.
+	 * 
+	 * @return A ResponseEntity containing the error response for the bad request
+	 *         with status code 500 (INTERNAL_SERVER_ERROR).
+	 */
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse>/* */
+			handlerAllException(Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = /* */
+				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	/**
+	 * Handles exceptions related to bad requests, specifically those caused by
+	 * unsupported mathematical operations.
+	 * 
+	 * @param ex      The exception to be handled.
+	 * @param request The HTTP request that resulted in the exception.
+	 * 
+	 * @return A ResponseEntity with HTTP status code 400 (Bad Request) containing
+	 *         the error response for the bad request.
+	 */
+	@ExceptionHandler(UnsupportedMathOperationException.class)
+	public final ResponseEntity<ExceptionResponse>/* */
+			handlerBadRequestExceptions(Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = /* */
+				new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+}
